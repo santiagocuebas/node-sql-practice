0958dd65b850f9f1247d8e77c9e8e43d05d1ccd5
Santiago Cuebas <sanlugus497@gmail.com>
2022-06-16 10:12:58 -0300

    Formulario añadido / Navbar comenzada

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,6 +5,10 @@ const router = Router();
 
 router.get('/add', (req,res) => {
 	res.render('links/add.hbs');
-})
+});
+
+router.post('/add', (req, res) => {
+	res.send('received');
+});
 
 export default router;
